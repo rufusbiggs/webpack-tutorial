c1ac99f6254789a2396952565e9693317782429d
Rufus B <127842229+rufusbiggs@users.noreply.github.com>
2023-12-18 13:02:03 +0000

    config complete

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    mode: 'development',
+    entry: './code/main.js',
+    rules: 
+        [
+            {
+                test: /\.css$/i,
+                use: ["style-loader","css-loader"]
+            },
+            {
+                test: /\.png$/i,
+                type: "asset/resource"
+            },
+            {
+                test: /\.txt$/i,
+                type: "asset/source"
+            },
+            {
+                test: /\.ttf$/i,
+                type: "asset/resource"
+            }
+        ],
+}
